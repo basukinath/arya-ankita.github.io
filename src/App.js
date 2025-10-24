15135871f3bbd20c708289902458ddcd1687cc8c
Ankita Arya <ankitaarya292@gmail.com>
2025-10-24 20:38:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Welcome from "./components/Welcome";
+import Work from "./components/Work";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+const AppLayout = () => {
+  return <div className="app">
+    <Header/>
+    <Welcome/>
+    <About/>
+    <Work/>
+    <Contact/>
+  </div>;
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
